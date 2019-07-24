d40948275c462a2ff722ef08a732b5c8572d7212
Jack Abukhovski <yabukhovski@ucdavis.edu>
2019-07-23 18:51:06 -0700

    Create LevelCtrl.java
    Created a LevelCtrl class that will handle the number of hornets on the screen, the number of missiles inside base, the number of power ups that are able to spawn and the level of the game.

---
+++ MissileCommand/app/src/main/java/com/example/misslecommand/LevelCtrl.java
@@ -0,0 +1,34 @@
+package com.example.misslecommand;
+
+public class LevelCtrl {
+
+    public int level;
+    public HornetCtrl hornets;
+    public int numHornets;
+    public int numMissiles;
+    public int numPowerups;
+    public int speedDivisor;    // Math will be calculated to take care of the speed of the falling hornets
+
+    public LevelCtrl(){
+        level = 1;
+        numHornets = 10;
+        numMissiles = 10;
+        numPowerups = 2;
+    }
+
+    public void controlLevel(){
+        if(numHornets == 0){
+            nextLevel();
+        }
+    }
+
+    public void nextLevel(){
+        level++;
+        numHornets += 2;
+        numMissiles += 1;
+        if(level % 2 == 0){
+            numPowerups += 1;
+        }
+    }
+
+}
