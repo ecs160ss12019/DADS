8e47ac3030787d5d600f296febdd6e1848d6750c
Shayan Mandegarian <smandegarian@yahoo.com>
2019-07-24 17:53:28 -0700

    quick low explosion fix again

--- MissileCommand/app/src/main/java/com/example/misslecommand/Base.java
+++ MissileCommand/app/src/main/java/com/example/misslecommand/Base.java
@@ -33,7 +33,7 @@ public class Base{
 
     //will create an instance of missile and then from TouchEvent fire it to a certain location
     public void fire(int xTouch, int yTouch){
-        if (ammo <= 0 || yTouch >= yBottom - 30) {
+        if (ammo <= 0 || yTouch > yTop - 10) {
             //out of ammo
             return;
         }
